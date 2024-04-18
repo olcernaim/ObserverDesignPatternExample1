ecd9464c7419a0e5ee2391750b7f1f94f0efe996
naimolcer <naimolcer@gmail.com>
2024-04-18 21:30:46 +0300

    Add project files.

---
+++ ObserverDesignPatternExample1/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverDesignPatternExample1
+{
+    class Product
+    {
+        private List<Observer> _observers = new List<Observer>();
+
+        public void Attach(Observer observer)
+        {
+            _observers.Add(observer);
+        }
+        public void Detach(Observer observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        private void Notify()
+        {
+            _observers.ForEach(o => o.Update());
+        }
+
+        public void ChangeStock()
+        {
+            this.Notify();
+        }
+    }
+}
---
+++ ObserverDesignPatternExample1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ObserverDesignPatternExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+
+            product.Attach(new CustomerObserver());
+            product.Attach(new CustomerObserver());
+            product.Attach(new CustomerObserver());
+
+            product.ChangeStock();
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
